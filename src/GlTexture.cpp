3e23524279d5f2954594b04118f04db9a6335a86
Thomas Müller <thomas94@gmx.net>
2017-08-27 14:43:24 +0200

    Use mipmap for zooming out of images

--- src/GlTexture.cpp
+++ src/GlTexture.cpp
@@ -38,10 +38,12 @@ void GlTexture::setData(const vector<float>& data, const Vector2i& size, int num
     );
 
     glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, size.x(), size.y(), 0, format, GL_FLOAT, data.data());
+    glGenerateMipmap(GL_TEXTURE_2D);
+
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 }
 
 TEV_NAMESPACE_END
