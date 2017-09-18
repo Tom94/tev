056f2e9d38cf8f517a56304f8db4b3e952b9c323
Thomas Müller <thomas94@gmx.net>
2017-09-18 18:58:39 +0200

    (Almost) Proper launching of tev via the command line on OS X

---
+++ scripts/osx-run-tev.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+realpath() {
+    # If the path starts with a slash, then it is already
+    # absolute. Otherwise, simply prepend current dir.
+    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
+# Transform all path arguments to absolute paths.
+# This is required, since opening an app in OS X
+# does not let us control the working directory of
+# the app, and relative paths may thus not work.
+paths=()
+for var in "$@"
+do
+    # Command-line arguments starting with - or :
+    # are not interpreted as paths by tev, so we
+    # should not expand them.
+    if  [[ $var == -* ]] || [[ $var == :* ]]; then
+        paths+=($var)
+    else
+        paths+=($(realpath $var))
+    fi
+done
+
+open -n -a tev --args "${paths[@]}"
