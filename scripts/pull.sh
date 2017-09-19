a200cd2ad3d824fbc33aaf6318da96beaff12486
Thomas Müller <thomas94@gmx.net>
2017-09-19 09:41:57 +0200

    Make git scripts use a fixed working directory

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+cd "$(dirname ${BASH_SOURCE[0]})/.."
 git pull
 ./scripts/update-submodules.sh
 
--- scripts/update-submodules.sh
+++ scripts/update-submodules.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+cd "$(dirname ${BASH_SOURCE[0]})/.."
 git submodule update --recursive
 git submodule sync --recursive
 git submodule update --recursive
