802f9099fc3abbf3aa5473696ce1bb8d9c682d31
Thomas Müller <thomas94@gmx.net>
2017-09-29 16:05:16 +0200

    Create backwards-compatible (10.9+) macOS releases

--- scripts/create-dmg.sh
+++ scripts/create-dmg.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
 cd "$(dirname ${BASH_SOURCE[0]})"
-RESULT="../tev.dmg"
 
+echo "Building backwards-compatible tev..."
+mkdir build-dmg && cd build-dmg
+MACOSX_DEPLOYMENT_TARGET=10.9
+cmake \
+    -DCMAKE_OSX_SYSROOT=/Users/tom94/Projects/MacOSX-SDKs/MacOSX10.9.sdk/ \
+    -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 \
+    ../..
+make -j
+cd ..
+
+echo "Creating dmg..."
+RESULT="../tev.dmg"
 test -f $RESULT && rm $RESULT
-./create-dmg/create-dmg --window-size 500 300 --icon-size 96 --volname "tev Installer" --app-drop-link 360 105 --icon tev.app 130 105 $RESULT /Applications/tev.app
+./create-dmg/create-dmg --window-size 500 300 --icon-size 96 --volname "tev Installer" --app-drop-link 360 105 --icon tev.app 130 105 $RESULT ./build-dmg/tev.app
+
+echo "Removing temporary build dir..."
+rm -rf build-dmg
