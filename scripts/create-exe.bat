36055c1457c04e01738abb3cb2b8d948e22ba87a
Thomas Müller <thomas94@gmx.net>
2017-09-30 10:06:38 +0200

    Add script for creating Windows executables for deployment

---
+++ scripts/create-exe.bat
@@ -0,0 +1,33 @@
+@echo off
+
+set cwd=%cd%
+cd /D %~dp0
+
+set DevCmd="C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat"
+set MSBuildOptions=/v:m /p:Configuration=Release
+set BuildDir64="build-exe-64"
+set BuildDir32="build-exe-32"
+
+call %DevCmd%
+
+echo Building 64-bit tev...
+mkdir %BuildDir64%
+cd %BuildDir64%
+cmake -G "Visual Studio 15 2017 Win64" ..\..
+msbuild %MSBuildOptions% tev.sln
+move "Release\tev.exe" "..\..\tev.exe"
+cd ..
+rmdir /S /Q %BuildDir64%
+
+echo Building 32-bit tev...
+mkdir %BuildDir32%
+cd %BuildDir32%
+cmake -G "Visual Studio 15 2017" ..\..
+msbuild %MSBuildOptions% tev.sln
+move "Release\tev.exe" "..\..\tev-32bit.exe"
+cd ..
+rmdir /S /Q %BuildDir32%
+
+echo Returning to original directory.
+cd /D %cwd%
+pause
