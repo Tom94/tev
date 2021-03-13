7b95e8af014c051f334bc4a33c969b7874742daa
Thomas Müller <thomas94@gmx.net>
2021-03-13 12:02:04 +0100

    Increase minimum cmake version to suppress deprecation warnings

--- scripts/mac-post-install.cmake
+++ scripts/mac-post-install.cmake
@@ -1,4 +1,4 @@
-cmake_minimum_required(VERSION 2.8.3)
+cmake_minimum_required(VERSION 2.8.12)
 
 # We want to explicitly symlink the newly installed app bundle
 # such that the icon and executable match up correctly.
