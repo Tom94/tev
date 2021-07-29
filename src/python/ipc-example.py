3a96b839cbe8a2506f4a07a9b2be4acf4ca82e72
Thomas Müller <thomas94@gmx.net>
2021-07-29 08:03:35 +0200

    Use the example to verify tiled image updates are working

--- src/python/ipc-example.py
+++ src/python/ipc-example.py
@@ -12,13 +12,13 @@ import numpy as np
 
 if __name__ == "__main__":
     with TevIpc() as tev:
-        image_data = np.full((100,100,3), 1.0)
+        image_data = np.full((300,200,3), 1.0)
         image_data[40:61,:,0] = 0.0
         image_data[:,40:61,1] = 0.0
         image_data[50:71,50:71,2] = 0.0
 
         bonus_data = image_data[:,:,0] + image_data[:,:,1] + image_data[:,:,2]
 
-        tev.create_image("Test Image", width=100, height=100, channel_names=["R","G","B","Bonus"])
+        tev.create_image("Test Image", width=200, height=300, channel_names=["R","G","B","Bonus"])
         tev.update_image("Test Image", image_data, ["R", "G", "B"])
         tev.update_image("Test Image", bonus_data, ["Bonus"])
