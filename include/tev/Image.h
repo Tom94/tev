86a0bb61e239707b64f8431677cc82ffd8e572ce
Thomas Müller <thomas94@gmx.net>
2021-08-15 16:29:42 +0200

    Image positioning based on display window rather than data window

--- include/tev/Image.h
+++ include/tev/Image.h
@@ -142,6 +142,10 @@ public:
         return mData.displayWindow;
     }
 
+    nanogui::Vector2f centerDisplayOffset() const {
+        return Box2f{dataWindow()}.middle() - Box2f{displayWindow()}.middle();
+    }
+
     size_t numPixels() const {
         return mData.numPixels();
     }
