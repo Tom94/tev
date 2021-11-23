42ca2d6b911265714b2d87a26ab77c889a051adf
Thomas Müller <thomas94@gmx.net>
2021-11-23 22:47:08 +0100

    Update macOS binary generation scripts

--- scripts/create-dmg-backwards.sh
+++ scripts/create-dmg-backwards.sh
@@ -6,19 +6,21 @@ echo "Building backwards-compatible tev..."
 
 BUILD_DIR="build-dmg"
 
-mkdir $BUILD_DIR && cd $BUILD_DIR
-MACOSX_DEPLOYMENT_TARGET=10.10
+mkdir $BUILD_DIR
+cd $BUILD_DIR
+MACOSX_DEPLOYMENT_TARGET=10.14
 cmake \
-    -DCMAKE_OSX_SYSROOT=/Users/tom94/Projects/MacOSX-SDKs/MacOSX10.10.sdk/ \
-    -DCMAKE_OSX_DEPLOYMENT_TARGET=10.10 \
+    -DCMAKE_OSX_SYSROOT=/Users/tom94/Projects/MacOSX-SDKs/MacOSX10.14.sdk/ \
+    -DCMAKE_OSX_DEPLOYMENT_TARGET=10.14 \
     -DTEV_DEPLOY=1 \
     ../..
 make -j
 cd ..
 
 echo "Creating dmg..."
-RESULT="../tev-pre-mojave.dmg"
-test -f $RESULT && rm $RESULT
+RESULT="../tev-pre-catalina.dmg"
+test -f $RESULT
+rm $RESULT
 ./create-dmg/create-dmg --window-size 500 300 --icon-size 96 --volname "tev Installer" --app-drop-link 360 105 --icon tev.app 130 105 $RESULT $BUILD_DIR/tev.app
 
 echo "Removing temporary build dir..."
--- scripts/create-dmg.sh
+++ scripts/create-dmg.sh
@@ -6,10 +6,11 @@ echo "Building tev..."
 
 BUILD_DIR="build-dmg"
 
-mkdir $BUILD_DIR && cd $BUILD_DIR
-MACOSX_DEPLOYMENT_TARGET=10.14
+mkdir $BUILD_DIR
+cd $BUILD_DIR
+MACOSX_DEPLOYMENT_TARGET=10.15
 cmake \
-    -DCMAKE_OSX_DEPLOYMENT_TARGET=10.14 \
+    -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 \
     -DTEV_DEPLOY=1 \
     ../..
 make -j
@@ -17,8 +18,9 @@ cd ..
 
 echo "Creating dmg..."
 RESULT="../tev.dmg"
-test -f $RESULT && rm $RESULT
-./create-dmg/create-dmg --window-size 500 300 --icon-size 96 --volname "tev Mojave Installer" --app-drop-link 360 105 --icon tev.app 130 105 $RESULT $BUILD_DIR/tev.app
+test -f $RESULT
+rm $RESULT
+./create-dmg/create-dmg --window-size 500 300 --icon-size 96 --volname "tev Installer" --app-drop-link 360 105 --icon tev.app 130 105 $RESULT $BUILD_DIR/tev.app
 
 echo "Removing temporary build dir..."
 rm -rf $BUILD_DIR
