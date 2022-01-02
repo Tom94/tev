a420bf744a3b904b572926ca6a336dc3146b9e4f
Thomas Müller-Höhne <thomas94@gmx.net>
2022-01-02 14:26:28 +0100

    Task<T> encapsulation

--- include/tev/Task.h
+++ include/tev/Task.h
@@ -50,7 +50,7 @@ void waitAll(std::vector<T>& futures) {
 struct DetachedTask {
     struct promise_type {
         DetachedTask get_return_object() noexcept {
-            return {COROUTINE_NAMESPACE::coroutine_handle<promise_type>::from_promise(*this)};
+            return {};
         }
 
         COROUTINE_NAMESPACE::suspend_never initial_suspend() const noexcept { return {}; }
@@ -66,8 +66,6 @@ struct DetachedTask {
             }
         }
     };
-
-    COROUTINE_NAMESPACE::coroutine_handle<promise_type> handle;
 };
 
 template <typename F, typename ...Args>
@@ -76,29 +74,23 @@ DetachedTask invokeTaskDetached(F&& executor, Args&&... args) {
     co_await exec(args...);
 }
 
-// The task implementation is inspired by a sketch from the following blog post:
-// https://www.jeremyong.com/cpp/2021/01/04/cpp20-coroutines-a-minimal-async-framework/
 template <typename data_t>
-struct TaskPromiseBase {
-    std::promise<data_t> promise;
+class TaskPromiseBase {
+public:
+    void return_value(data_t&& value) noexcept { mPromise.set_value(std::move(value)); }
+    void return_value(const data_t& value) noexcept { mPromise.set_value(value); }
 
-    // When the coroutine co_returns a value, this method is used to publish the result
-    void return_value(data_t&& value) noexcept {
-        promise.set_value(std::move(value));
-    }
-
-    void return_value(const data_t& value) noexcept {
-        promise.set_value(value);
-    }
+protected:
+    std::promise<data_t> mPromise;
 };
 
 template <>
-struct TaskPromiseBase<void> {
-    std::promise<void> promise;
+class TaskPromiseBase<void> {
+public:
+    void return_void() noexcept { mPromise.set_value(); }
 
-    void return_void() noexcept {
-        promise.set_value();
-    }
+protected:
+    std::promise<void> mPromise;
 };
 
 struct TaskSharedState {
@@ -107,61 +99,63 @@ struct TaskSharedState {
 };
 
 template <typename future_t, typename data_t>
-struct TaskPromise : public TaskPromiseBase<data_t> {
-    std::shared_ptr<TaskSharedState> state = std::make_shared<TaskSharedState>();
-
+class TaskPromise : public TaskPromiseBase<data_t> {
+public:
     future_t get_return_object() noexcept {
-        return {COROUTINE_NAMESPACE::coroutine_handle<TaskPromise<future_t, data_t>>::from_promise(*this), this->promise.get_future(), state};
+        return {COROUTINE_NAMESPACE::coroutine_handle<TaskPromise<future_t, data_t>>::from_promise(*this), this->mPromise.get_future(), mState};
     }
 
     COROUTINE_NAMESPACE::suspend_never initial_suspend() const noexcept { return {}; }
 
     void unhandled_exception() {
-        this->promise.set_exception(std::current_exception());
+        this->mPromise.set_exception(std::current_exception());
     }
 
     // The coroutine is about to complete (via co_return, reaching the end of the coroutine body,
     // or an uncaught exception). The awaiter returned here defines what happens next
     auto final_suspend() noexcept {
-        struct Awaiter {
-            COROUTINE_NAMESPACE::coroutine_handle<> continuation;
+        class Awaiter {
+        public:
+            Awaiter(COROUTINE_NAMESPACE::coroutine_handle<> continuation) : mContinuation{continuation} {}
 
-            bool await_ready() noexcept { return !continuation; }
+            bool await_ready() noexcept { return !mContinuation; }
             void await_resume() noexcept {}
 
             // Returning the continuation has the effect of continuing execution where the parent co_await'ed us.
             // It's the parent's job to call destroy on this coroutine's handle.
             COROUTINE_NAMESPACE::coroutine_handle<> await_suspend(COROUTINE_NAMESPACE::coroutine_handle<TaskPromise<future_t, data_t>>) noexcept {
-                return continuation;
+                return mContinuation;
             }
+
+        private:
+            COROUTINE_NAMESPACE::coroutine_handle<> mContinuation;
         };
 
-        bool isLast = state->latch.countDown();
-        return Awaiter{isLast ? state->continuation : nullptr};
+        bool isLast = mState->latch.countDown();
+        return Awaiter{isLast ? mState->continuation : nullptr};
     }
+
+private:
+    std::shared_ptr<TaskSharedState> mState = std::make_shared<TaskSharedState>();
 };
 
 template <typename T>
-struct Task {
+class Task {
+public:
     using promise_type = TaskPromise<Task<T>, T>;
 
-    // This handle is assigned to when the coroutine itself is suspended (see await_suspend above)
-    COROUTINE_NAMESPACE::coroutine_handle<promise_type> handle;
-    std::future<T> future;
-    std::shared_ptr<TaskSharedState> state;
-
     Task(COROUTINE_NAMESPACE::coroutine_handle<promise_type> handle, std::future<T>&& future, const std::shared_ptr<TaskSharedState>& state)
-    : handle{handle}, future{std::move(future)}, state{state} {}
+    : mHandle{handle}, mFuture{std::move(future)}, mState{state} {}
 
     // No copying allowed!
     Task(const Task& other) = delete;
     Task& operator=(const Task& other) = delete;
 
     Task& operator=(Task&& other) {
-        handle = other.handle;
-        other.handle = nullptr;
-        future = std::move(other.future);
-        state = std::move(other.state);
+        mHandle = other.mHandle;
+        other.mHandle = nullptr;
+        mFuture = std::move(other.mFuture);
+        mState = std::move(other.mState);
         return *this;
     }
     Task(Task&& other) {
@@ -170,7 +164,7 @@ struct Task {
 
     ~Task() {
         // Make sure the coroutine finished and is cleaned up
-        if (handle) {
+        if (mHandle) {
             tlog::warning() << "~Task<T> was invoked before completion.";
         }
     }
@@ -179,9 +173,9 @@ struct Task {
         // If the latch has already been passed by final_suspend()
         // above, the task has already completed and we can skip
         // suspension of the coroutine.
-        if (state->latch.value() <= 1) {
-            state->latch.countDown();
-            handle = nullptr;
+        if (mState->latch.value() <= 1) {
+            mState->latch.countDown();
+            mHandle = nullptr;
             return true;
         }
 
@@ -193,42 +187,47 @@ struct Task {
         // this task's own coroutine handle has not been cleaned up (for
         // implementation reasons) and needs to be destroyed here.
         // (See the behavior of final_suspend() above.)
-        if (handle) {
-            handle.destroy();
-            handle = nullptr;
+        if (mHandle) {
+            mHandle.destroy();
+            mHandle = nullptr;
         }
 
         // Note: if there occurred an uncaught exception while executing
         // this task, it'll get rethrown in the following call.
-        return future.get();
+        return mFuture.get();
     }
 
     T get() {
-        if (!handle) {
+        if (!mHandle) {
             tlog::error() << "Cannot get()/co_await a task multiple times.";
         }
 
-        handle = nullptr;
+        mHandle = nullptr;
         return await_resume();
     }
 
     bool await_suspend(COROUTINE_NAMESPACE::coroutine_handle<> coroutine) noexcept {
-        if (!handle) {
+        if (!mHandle) {
             tlog::error() << "Cannot co_await/get() a task multiple times.";
             std::terminate();
         }
 
         // If the task is still running (checked by arriving at the latch),
         // mark this coroutine as the task's continuation and suspend it until then.
-        // The task's coroutine `handle` remains valid if suspended, implying that it
+        // The task's coroutine `mHandle` remains valid if suspended, implying that it
         // needs to be manually cleaned up on resumption of this coroutine (see await_resume()).
-        state->continuation = coroutine;
-        bool shallSuspend = !state->latch.countDown();
+        mState->continuation = coroutine;
+        bool shallSuspend = !mState->latch.countDown();
         if (!shallSuspend) {
-            handle = nullptr;
+            mHandle = nullptr;
         }
         return shallSuspend;
     }
+
+private:
+    COROUTINE_NAMESPACE::coroutine_handle<promise_type> mHandle;
+    std::future<T> mFuture;
+    std::shared_ptr<TaskSharedState> mState;
 };
 
 TEV_NAMESPACE_END
