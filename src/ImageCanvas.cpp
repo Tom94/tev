4e696a7c125d855cfcef5c2deb85a83f7f2f3094
Thomas Müller-Höhne <thomas94@gmx.net>
2022-11-28 21:21:53 +0900

    ImageCanvas: fix incorrect vector graphics coordinates

--- src/ImageCanvas.cpp
+++ src/ImageCanvas.cpp
@@ -197,10 +197,10 @@ void ImageCanvas::drawCoordinateSystem(NVGcontext* ctx) {
         float fontSize = 20;
         float strokeWidth = 3.0f;
 
-        Vector2i topLeft     = m_pos + Vector2i{displayWindowToNano * Vector2f{window.min.x(), window.min.y()}};
-        Vector2i topRight    = m_pos + Vector2i{displayWindowToNano * Vector2f{window.max.x(), window.min.y()}};
-        Vector2i bottomLeft  = m_pos + Vector2i{displayWindowToNano * Vector2f{window.min.x(), window.max.y()}};
-        Vector2i bottomRight = m_pos + Vector2i{displayWindowToNano * Vector2f{window.max.x(), window.max.y()}};
+        Vector2f topLeft     = Vector2f{m_pos} + displayWindowToNano * Vector2f{window.min.x(), window.min.y()};
+        Vector2f topRight    = Vector2f{m_pos} + displayWindowToNano * Vector2f{window.max.x(), window.min.y()};
+        Vector2f bottomLeft  = Vector2f{m_pos} + displayWindowToNano * Vector2f{window.min.x(), window.max.y()};
+        Vector2f bottomRight = Vector2f{m_pos} + displayWindowToNano * Vector2f{window.max.x(), window.max.y()};
 
         nvgSave(ctx);
 
@@ -302,24 +302,30 @@ void ImageCanvas::draw(NVGcontext* ctx) {
     if (mImage) {
         drawPixelValuesAsText(ctx);
 
+        auto displayWindowToNano = displayWindowToNanogui(mImage.get());
+
+        auto vgToNano = [&](const VgCommand::Pos& p) {
+            return Vector2f{m_pos} + displayWindowToNano * Vector2f{p.x, p.y};
+        };
+
         auto applyVgCommand = [&](const VgCommand& command) {
             switch (command.type) {
                 case VgCommand::EType::Save: nvgSave(ctx); return;
                 case VgCommand::EType::Restore: nvgRestore(ctx); return;
                 case VgCommand::EType::MoveTo: {
-                    const VgCommand::Pos& p = *reinterpret_cast<const VgCommand::Pos*>(command.data.data());
-                    nvgMoveTo(ctx, p.x, p.y);
+                    Vector2f p = vgToNano(*reinterpret_cast<const VgCommand::Pos*>(command.data.data()));
+                    nvgMoveTo(ctx, p.x(), p.y());
                 } return;
                 case VgCommand::EType::LineTo: {
-                    const VgCommand::Pos& p = *reinterpret_cast<const VgCommand::Pos*>(command.data.data());
-                    nvgLineTo(ctx, p.x, p.y);
+                    Vector2f p = vgToNano(*reinterpret_cast<const VgCommand::Pos*>(command.data.data()));
+                    nvgLineTo(ctx, p.x(), p.y());
                 } return;
                 case VgCommand::EType::FillColor: {
-                    const VgCommand::Color& c = *reinterpret_cast<const VgCommand::Color*>(command.data.data());
+                    VgCommand::Color c = *reinterpret_cast<const VgCommand::Color*>(command.data.data());
                     nvgFillColor(ctx, {{{c.r, c.g, c.b, c.a}}});
                 } return;
                 case VgCommand::EType::StrokeColor: {
-                    const VgCommand::Color& c = *reinterpret_cast<const VgCommand::Color*>(command.data.data());
+                    VgCommand::Color c = *reinterpret_cast<const VgCommand::Color*>(command.data.data());
                     nvgStrokeColor(ctx, {{{c.r, c.g, c.b, c.a}}});
                 } return;
                 case VgCommand::EType::Fill: nvgFill(ctx); return;
