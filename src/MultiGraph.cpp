721385ffc4dac321449cd0abc47babc32cd6bc53
Thomas Müller <git@tom94.net>
2025-02-25 07:30:24 +0100

    SWITCH TO GPLv3 LICENSE
    As I'm adding more features to tev, more and more dependencies become
    more ergonomic to use if tev is under the GPLv3 license. Over the years,
    I've also become more and more appreciative of the values that
    free-software licenses represent, so this is a good opportunity to make
    the switch. tev's version will get bumped to 2.0 to reflect this major
    change in licensing.

--- src/MultiGraph.cpp
+++ src/MultiGraph.cpp
@@ -1,7 +1,24 @@
+/*
+ * tev -- the EXR viewer
+ *
+ * Copyright (C) 2025 Thomas Müller <contact@tom94.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 // This file was adapted from the nanogui::Graph class, which was developed
 // by Wenzel Jakob <wenzel.jakob@epfl.ch> and based on the NanoVG demo application
 // by Mikko Mononen. Modifications were developed by Thomas Müller <contact@tom94.net>.
-// This file is published under the BSD 3-Clause License within the LICENSE file.
 
 #include <tev/MultiGraph.h>
 
