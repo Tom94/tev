f6c08de1a6687abee153e39a1bfca347c4cc81bc
Thomas Müller <git@tom94.net>
2025-05-02 09:11:32 +0200

    feat: generalize ICC color conversion for use in more image loaders
    - Color conversion now supports grayscale and 2 channel images
    - Calls into lcms2 are now properly threadsafe (previously was a big
    janky)
    - The extra threadsafety allows for minor optimizations. Measured
    speedup of end-to-end HEIF loading is around 5% faster.

--- include/tev/imageio/Chroma.h
+++ include/tev/imageio/Chroma.h
@@ -19,6 +19,7 @@
 #pragma once
 
 #include <tev/Common.h>
+#include <tev/Task.h>
 
 #include <nanogui/vector.h>
 
@@ -28,4 +29,23 @@ namespace tev {
 
 nanogui::Matrix4f convertChromaToRec709(std::array<nanogui::Vector2f, 4> chroma);
 
-}
+enum class EAlphaKind { Premultiplied, PremultipliedLinear, Straight, None };
+
+// Converts colors from an ICC profile to linear sRGB Rec.709 w/ *straight* alpha. We would have preferred to convert in premultiplied
+// space, but the underlying color management library (lcms2) does not support this. Hence, callers of this function are responsible to
+// remultiply alpha if desired.
+//
+// Note that, because we this function converts potentially larger color gamuts to sRGB, output channels may have values larger than 1 or
+// smaller than 0, even if the input was within [0, 1]. This is by design, and underlying rendering APIs, like OpenGL and Metal will
+// correctly display such colors on HDR displays.
+Task<void> convertIccToRec709(
+    const std::vector<uint8_t>& iccProfile,
+    const nanogui::Vector2i& size,
+    int numColorChannels,
+    EAlphaKind alphaKind,
+    float* __restrict src,
+    float* __restrict rgbaDst,
+    int priority
+);
+
+} // namespace tev
