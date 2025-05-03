c84c2bb4e301bdacc6fc4f38d0f2d85b052cd218
Thomas Müller <git@tom94.net>
2025-05-03 15:07:21 +0200

    feat: gif: load all gif frames rather than just the first

--- src/imageio/StbiImageLoader.cpp
+++ src/imageio/StbiImageLoader.cpp
@@ -19,6 +19,7 @@
 #include <tev/ThreadPool.h>
 #include <tev/imageio/StbiImageLoader.h>
 
+#define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
 
 using namespace nanogui;
@@ -42,6 +43,7 @@ Task<vector<ImageData>> StbiImageLoader::load(istream& iStream, const fs::path&,
 
     void* data;
     int numChannels;
+    int numFrames = 1;
     Vector2i size;
     bool isHdr = stbi_is_hdr_from_callbacks(&callbacks, &iStream) != 0;
     iStream.clear();
@@ -50,11 +52,27 @@ Task<vector<ImageData>> StbiImageLoader::load(istream& iStream, const fs::path&,
     if (isHdr) {
         data = stbi_loadf_from_callbacks(&callbacks, &iStream, &size.x(), &size.y(), &numChannels, 0);
     } else {
-        data = stbi_load_from_callbacks(&callbacks, &iStream, &size.x(), &size.y(), &numChannels, 0);
+        stbi__context s;
+        stbi__start_callbacks(&s, (stbi_io_callbacks*)&callbacks, &iStream);
+        bool isGif = stbi__gif_test(&s);
+        iStream.clear();
+        iStream.seekg(0);
+
+        if (isGif) {
+            stbi__start_callbacks(&s, (stbi_io_callbacks*)&callbacks, &iStream);
+            int* delays; // We don't care about gif frame delays. Read and discard.
+            data = stbi__load_gif_main(&s, &delays, &size.x(), &size.y(), &numFrames, &numChannels, 0);
+        } else {
+            data = stbi_load_from_callbacks(&callbacks, &iStream, &size.x(), &size.y(), &numChannels, 0);
+        }
     }
 
     if (!data) {
         throw invalid_argument{std::string{stbi_failure_reason()}};
+    }
+
+    if (numFrames == 0) {
+        throw invalid_argument{"Image has zero frames."};
     }
 
     if (size.x() == 0 || size.y() == 0) {
@@ -63,46 +81,55 @@ Task<vector<ImageData>> StbiImageLoader::load(istream& iStream, const fs::path&,
 
     ScopeGuard dataGuard{[data] { stbi_image_free(data); }};
 
-    vector<ImageData> result(1);
-    ImageData& resultData = result.front();
+    vector<ImageData> result(numFrames);
+    for (size_t frameIdx = 0; frameIdx < numFrames; ++frameIdx) {
+        ImageData& resultData = result[frameIdx];
 
-    resultData.channels = makeNChannels(numChannels, size);
-    static const int ALPHA_CHANNEL_INDEX = 3;
+        resultData.channels = makeNChannels(numChannels, size);
+        resultData.hasPremultipliedAlpha = false;
+        if (numFrames > 1) {
+            resultData.partName = fmt::format("frames.{}", frameIdx);
+        }
 
-    auto numPixels = (size_t)size.x() * size.y();
-    if (isHdr) {
-        co_await ThreadPool::global().parallelForAsync<size_t>(
-            0,
-            numPixels,
-            [&](size_t i) {
-                auto typedData = reinterpret_cast<float*>(data);
-                size_t baseIdx = i * numChannels;
-                for (int c = 0; c < numChannels; ++c) {
-                    resultData.channels[c].at(i) = typedData[baseIdx + c];
-                }
-            },
-            priority
-        );
-    } else {
-        co_await ThreadPool::global().parallelForAsync<size_t>(
-            0,
-            numPixels,
-            [&](size_t i) {
-                auto typedData = reinterpret_cast<unsigned char*>(data);
-                size_t baseIdx = i * numChannels;
-                for (int c = 0; c < numChannels; ++c) {
-                    if (c == ALPHA_CHANNEL_INDEX) {
-                        resultData.channels[c].at(i) = (typedData[baseIdx + c]) / 255.0f;
-                    } else {
-                        resultData.channels[c].at(i) = toLinear((typedData[baseIdx + c]) / 255.0f);
+        static const int ALPHA_CHANNEL_INDEX = 3;
+
+        auto numPixels = (size_t)size.x() * size.y();
+        if (isHdr) {
+            auto typedData = reinterpret_cast<float*>(data);
+            co_await ThreadPool::global().parallelForAsync<size_t>(
+                0,
+                numPixels,
+                [&](size_t i) {
+                    size_t baseIdx = i * numChannels;
+                    for (int c = 0; c < numChannels; ++c) {
+                        resultData.channels[c].at(i) = typedData[baseIdx + c];
                     }
-                }
-            },
-            priority
-        );
-    }
+                },
+                priority
+            );
 
-    resultData.hasPremultipliedAlpha = false;
+            data = typedData + numPixels * numChannels;
+        } else {
+            auto typedData = reinterpret_cast<unsigned char*>(data);
+            co_await ThreadPool::global().parallelForAsync<size_t>(
+                0,
+                numPixels,
+                [&](size_t i) {
+                    size_t baseIdx = i * numChannels;
+                    for (int c = 0; c < numChannels; ++c) {
+                        if (c == ALPHA_CHANNEL_INDEX) {
+                            resultData.channels[c].at(i) = (typedData[baseIdx + c]) / 255.0f;
+                        } else {
+                            resultData.channels[c].at(i) = toLinear((typedData[baseIdx + c]) / 255.0f);
+                        }
+                    }
+                },
+                priority
+            );
+
+            data = typedData + numPixels * numChannels;
+        }
+    }
 
     co_return result;
 }
