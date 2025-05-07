a3b6ab31991e0dfa3ebf86a8dbed7d1ac3bf40ab
Thomas Müller <git@tom94.net>
2025-05-07 06:38:30 -0700

    chore: move os-specific resource data into os-named folders

--- scripts/configure-winget.cmake
+++ scripts/configure-winget.cmake
@@ -10,4 +10,4 @@ set(TEV_VERSION "@TEV_VERSION@")
 list(GET CPACK_INSTALL_CMAKE_PROJECTS 0 TEV_BUILD_DIR)
 
 file(SHA256 "${CPACK_PACKAGE_FILES}" TEV_INSTALLER_SHA256)
-configure_file("${TEV_BUILD_DIR}/resources/winget.yaml" "${TEV_BUILD_DIR}/tev.yaml")
+configure_file("${TEV_BUILD_DIR}/resources/windows/winget.yaml" "${TEV_BUILD_DIR}/tev.yaml")
