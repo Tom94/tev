dc48dc63def013d5dded8abe38754a6b9f05f7b7
Thomas Müller <git@tom94.net>
2025-08-18 18:25:38 +0200

    feat(tiff): support 9-11 bit jpeg compression & more DNG color features

--- include/tev/imageio/Exif.h
+++ include/tev/imageio/Exif.h
@@ -31,6 +31,33 @@ struct _ExifLog;
 
 namespace tev {
 
+enum EExifLightSource : uint16_t {
+    Unknown = 0,
+    Daylight = 1,
+    Fluorescent = 2,
+    TungstenIncandescent = 3,
+    Flash = 4,
+    FineWeather = 9,
+    Cloudy = 10,
+    Shade = 11,
+    DaylightFluorescent = 12,
+    DayWhiteFluorescent = 13,
+    CoolWhiteFluorescent = 14,
+    WhiteFluorescent = 15,
+    WarmWhiteFluorescent = 16,
+    StandardLightA = 17,
+    StandardLightB = 18,
+    StandardLightC = 19,
+    D55 = 20,
+    D65 = 21,
+    D75 = 22,
+    D50 = 23,
+    ISOStudioTungsten = 24,
+    Other = 255,
+};
+
+std::string toString(EExifLightSource lightSource);
+
 class Exif {
 public:
     static constexpr std::array<uint8_t, 6> FOURCC = {
