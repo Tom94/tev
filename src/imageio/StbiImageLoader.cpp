5485e6d639ad0599f465b685d89e3c27d1db5b50
Thomas Müller <git@tom94.net>
2025-12-03 10:36:27 +0100

    fix(stbi): superfluous include

--- src/imageio/StbiImageLoader.cpp
+++ src/imageio/StbiImageLoader.cpp
@@ -16,7 +16,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "tev/imageio/Colors.h"
 #include <tev/ThreadPool.h>
 #include <tev/imageio/StbiImageLoader.h>
 
