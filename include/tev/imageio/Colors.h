2e1993b336317fd72910691de68d028ce1f5e8fb
Thomas Müller <git@tom94.net>
2026-01-08 15:27:10 +0100

    feat(colors): implement all forward transfer functions

--- include/tev/imageio/Colors.h
+++ include/tev/imageio/Colors.h
@@ -45,9 +45,7 @@ chroma_t zeroChroma();
 nanogui::Matrix3f xyzToChromaMatrix(const chroma_t& chroma);
 nanogui::Matrix3f adaptWhiteBradford(const nanogui::Vector2f& srcWhite, const nanogui::Vector2f& dstWhite);
 
-nanogui::Matrix3f convertColorspaceMatrix(
-    const chroma_t& srcChroma, const chroma_t& dstChroma, ERenderingIntent intent
-);
+nanogui::Matrix3f convertColorspaceMatrix(const chroma_t& srcChroma, const chroma_t& dstChroma, ERenderingIntent intent);
 
 nanogui::Vector2f whiteD50();
 nanogui::Vector2f whiteD55();
@@ -161,28 +159,47 @@ bool isTransferImplemented(const ETransfer transfer);
 
 ETransfer fromWpTransfer(int wpTransfer);
 
+namespace bt709 {
+static constexpr float beta = 0.018053968510807f;
+static constexpr float alpha = 1.0f + 5.5f * beta;
+static constexpr float thres = 4.5f * beta;
+} // namespace bt709
+
 inline float bt709ToLinear(float val) {
-    constexpr float beta = 0.018053968510807f;
-    constexpr float alpha = 1.0f + 5.5f * beta;
-    constexpr float thres = 4.5f * beta;
-    return val <= thres ? (val / 4.5f) : std::pow((val + alpha - 1.0f) / alpha, 1.0f / 0.45f);
+    return val <= bt709::thres ? (val / 4.5f) : std::pow((val + bt709::alpha - 1.0f) / bt709::alpha, 1.0f / 0.45f);
+}
+
+inline float linearToBt709(float val) {
+    return val <= bt709::beta ? (val * 4.5f) : (bt709::alpha * std::pow(val, 0.45f) - (bt709::alpha - 1.0f));
 }
 
 // From https://www.itu.int/dms_pubrec/itu-r/rec/bt/R-REC-BT.1361-0-199802-W!!PDF-E.pdf, generalized to the more precise constants from the
 // bt709ToLinear function as defined in https://www.itu.int/rec/T-REC-H.273-202407-I/en.
 inline float bt1361ExtendedToLinear(float val) {
-    constexpr float beta = 0.018053968510807f;
-    constexpr float alpha = 1.0f + 5.5f * beta;
-    constexpr float thres = 4.5f * beta;
-    constexpr float negThres = -thres / 4.0f;
+    constexpr float negThres = -bt709::thres / 4.0f;
 
     float result;
     if (val < negThres) {
-        result = -std::pow((-val * 4.0f + alpha - 1.0f) / alpha, 1.0f / 0.45f) / 4.0f;
-    } else if (val <= thres) {
+        result = (-1.0f / 4.0f) * std::pow((-4.0f * val + bt709::alpha - 1.0f) / bt709::alpha, 1.0f / 0.45f);
+    } else if (val <= bt709::thres) {
         result = val / 4.5f;
     } else {
-        result = std::pow((val + alpha - 1.0f) / alpha, 1.0f / 0.45f);
+        result = std::pow((val + bt709::alpha - 1.0f) / bt709::alpha, 1.0f / 0.45f);
+    }
+
+    return result;
+}
+
+inline float linearToBt1361Extended(float val) {
+    constexpr float negThres = -bt709::beta / 4.0f;
+
+    float result;
+    if (val < negThres) {
+        result = (-1.0f / 4.0f) * (bt709::alpha * std::pow(-4.0f * val, 0.45f) - (bt709::alpha - 1.0f));
+    } else if (val <= bt709::beta) {
+        result = val * 4.5f;
+    } else {
+        result = bt709::alpha * std::pow(val, 0.45f) - (bt709::alpha - 1.0f);
     }
 
     return result;
@@ -190,40 +207,74 @@ inline float bt1361ExtendedToLinear(float val) {
 
 // From http://car.france3.mars.free.fr/HD/INA-%2026%20jan%2006/SMPTE%20normes%20et%20confs/s240m.pdf
 inline float smpteSt240ToLinear(float val) { return val <= 0.0913f ? (val / 4.0f) : pow((val + 0.1115f) / 1.1115f, 1.0f / 0.45f); }
+inline float linearToSmpteSt240(float val) { return val <= 0.022825f ? (val * 4.0f) : (1.1115f * pow(val, 0.45f) - 0.1115f); }
+
+namespace pq {
+static constexpr float c1 = 107.0f / 128.0f;
+static constexpr float c2 = 2413.0f / 128.0f;
+static constexpr float c3 = 2392.0f / 128.0f;
+static constexpr float m1 = 1305.0f / 8192.0f;
+static constexpr float m2 = 2523.0f / 32.0f;
+static constexpr float invm1 = 8192.0f / 1305.0f;
+static constexpr float invm2 = 32.0f / 2523.0f;
+} // namespace pq
 
 inline float pqToLinear(float val) {
-    constexpr float c1 = 107.0f / 128.0f;
-    constexpr float c2 = 2413.0f / 128.0f;
-    constexpr float c3 = 2392.0f / 128.0f;
-    constexpr float invm1 = 8192.0f / 1305.0f;
-    constexpr float invm2 = 32.0f / 2523.0f;
+    const float tmp = std::pow(std::max(val, 0.0f), pq::invm2);
+    return 10000.0f / 203.0f * std::pow(std::max(tmp - pq::c1, 0.0f) / std::max(pq::c2 - pq::c3 * tmp, 1e-5f), pq::invm1);
+}
 
-    const float tmp = std::pow(std::max(val, 0.0f), invm2);
-    return 10000.0f / 203.0f * std::pow(std::max(tmp - c1, 0.0f) / std::max(c2 - c3 * tmp, 1e-5f), invm1);
+inline float linearToPq(float val) {
+    val = val * 203.0f / 10000.0f;
+    const float p = std::pow(std::max(val, 0.0f), pq::m1);
+
+    const float num = pq::c1 + pq::c2 * p;
+    return std::pow(num / (1.0f + pq::c3 * p), pq::m2);
 }
 
 inline float smpteSt428ToLinear(float val) { return std::pow(val, 2.6f) * (52.37f / 48.0f); }
+inline float linearToSmpteSt428(float val) { return std::pow(val * (48.0f / 52.37f), 1.0f / 2.6f); }
+
+namespace hlg {
+// TODO: make these params configurable at runtime
+static constexpr float Lw = 1000.0f; // display peak brightness in cd/m² (nits)
+static constexpr float gain = Lw; // can technically be adjusted, but usually set to Lw
+static const float gamma = 1.2f + 0.42f * std::log10(Lw / 1000.0f);
+
+static constexpr float a = 0.17883277f;
+static constexpr float b = 0.28466892f;
+static constexpr float c = 0.55991073f;
+} // namespace hlg
 
 inline nanogui::Vector3f hlgToLinear(const nanogui::Vector3f& val) {
     const auto invOetf = [](const float val) {
-        constexpr float a = 0.17883277f;
-        constexpr float b = 0.28466892f;
-        constexpr float c = 0.55991073f;
-        return val <= 0.5f ? (val * val / 3.0f) : ((std::exp((val - c) / a) + b) / 12.0f);
+        return val <= 0.5f ? (val * val / 3.0f) : ((std::exp((val - hlg::c) / hlg::a) + hlg::b) / 12.0f);
     };
 
     const auto ootf = [](const nanogui::Vector3f& val) {
-        // TODO: make these params configurable
-        constexpr float Lw = 1000.0; // display peak brightness in cd/m² (nits)
-        constexpr float gain = Lw; // can technically be adjusted, but usually set to Lw
-        const float gamma = 1.2f + 0.42f * std::log10(Lw / 1000.0f);
-
         // NOTE: HLG (BT.2100) mandates the use of Rec. 2020 primaries, so the following equation should always be valid.
         const float lum = 0.2627f * val.x() + 0.6780f * val.y() + 0.0593f * val.z();
-        return gain * pow(lum, gamma - 1.0f) * val;
+        return hlg::gain * pow(lum, hlg::gamma - 1.0f) * val;
     };
 
-    return ootf({invOetf(val.x()), invOetf(val.y()), invOetf(val.z())}) / 203.0f; // Convert to linear sRGB units where SDR white is 1.0
+    return ootf({invOetf(val.x()), invOetf(val.y()), invOetf(val.z())}) / 203.0f; // Convert to linear units where SDR white is 1.0
+}
+
+inline nanogui::Vector3f linearToHlg(const nanogui::Vector3f& val) {
+    const auto oetf = [](const float val) {
+        return val <= 1.0f / 12.0f ? std::sqrt(3.0f * val) : (hlg::a * std::log(12.0f * val - hlg::b) + hlg::c);
+    };
+
+    const auto invOotf = [](const nanogui::Vector3f& val) {
+        const auto tmp = val / hlg::gain;
+
+        // NOTE: HLG (BT.2100) mandates the use of Rec. 2020 primaries, so the following equation should always be valid.
+        const float lum = 0.2627f * tmp.x() + 0.6780f * tmp.y() + 0.0593f * tmp.z();
+        return pow(lum, (1.0f - hlg::gamma) / hlg::gamma) * tmp;
+    };
+
+    const auto tmp = invOotf(val * 203.0f); // Convert from linear units where SDR white is 1.0;
+    return {oetf(tmp.x()), oetf(tmp.y()), oetf(tmp.z())};
 }
 
 inline float invTransferComponent(const ETransfer transfer, float val) noexcept {
@@ -245,7 +296,7 @@ inline float invTransferComponent(const ETransfer transfer, float val) noexcept 
         case ETransfer::SRGB: return toLinear(val);
         case ETransfer::PQ: return pqToLinear(val);
         case ETransfer::SMPTE428: return smpteSt428ToLinear(val);
-        case ETransfer::HLG: return val; // Should be handled by invTransfer below
+        case ETransfer::HLG: return val; // Should be handled by `invTransfer()` below
         case ETransfer::Unspecified: return val; // Default to linear if unspecified
     }
 
@@ -265,13 +316,52 @@ inline nanogui::Vector3f invTransfer(const ETransfer transfer, const nanogui::Ve
     }
 }
 
+inline float transferComponent(const ETransfer transfer, float val) noexcept {
+    switch (transfer) {
+        case ETransfer::BT709:
+        case ETransfer::BT601:
+        case ETransfer::BT202010bit:
+        case ETransfer::BT202012bit: return linearToBt709(val);
+        case ETransfer::IEC61966_2_4: // handles negative values by mirroring
+            return std::copysign(linearToBt709(std::abs(val)), val);
+        case ETransfer::BT1361Extended: // extended to negative values (weirdly)
+            return linearToBt1361Extended(val);
+        case ETransfer::Gamma22: return std::pow(std::max(val, 0.0f), 1.0f / 2.2f);
+        case ETransfer::Gamma28: return std::pow(std::max(val, 0.0f), 1.0f / 2.8f);
+        case ETransfer::SMPTE240: return linearToSmpteSt240(val);
+        case ETransfer::Linear: return val;
+        case ETransfer::Log100: return val >= 0.01f ? 1.0f + std::log10(val) / 2.0f : 0.0f;
+        case ETransfer::Log100Sqrt10: return val >= std::sqrt(10.0f) / 1000.0f ? 1.0f + std::log10(val) / 2.5f : 0.0f;
+        case ETransfer::SRGB: return toSRGB(val);
+        case ETransfer::PQ: return linearToPq(val);
+        case ETransfer::SMPTE428: return linearToSmpteSt428(val);
+        case ETransfer::HLG: return val; // Should be handled by `transfer()` below
+        case ETransfer::Unspecified: return val; // Default to linear if unspecified
+    }
+
+    // Other transfer functions are not implemented. Default to linear.
+    return val;
+}
+
+inline nanogui::Vector3f transfer(const ETransfer transfer, const nanogui::Vector3f& val) noexcept {
+    if (transfer == ETransfer::HLG) {
+        return linearToHlg(val);
+    } else {
+        return {
+            transferComponent(transfer, val.x()),
+            transferComponent(transfer, val.y()),
+            transferComponent(transfer, val.z()),
+        };
+    }
+}
+
 inline float bestGuessReferenceWhiteLevel(const ETransfer transfer) {
     switch (transfer) {
         case ETransfer::PQ:
         case ETransfer::HLG: return 203.0f;
 
         case ETransfer::BT709: // 100 nits by convention, see e.g.
-                                              // https://partnerhelp.netflixstudios.com/hc/en-us/articles/360000591787-Color-Critical-Display-Calibration-Guidelines
+                               // https://partnerhelp.netflixstudios.com/hc/en-us/articles/360000591787-Color-Critical-Display-Calibration-Guidelines
         case ETransfer::BT601: // same as BT709 in practice
         case ETransfer::BT1361Extended: // Extends BT709 and inherits conventions.
         case ETransfer::IEC61966_2_4: // xvYCC proposed by sony. Extends BT709 and inherits conventions.
