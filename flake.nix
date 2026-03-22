fcd5f910e6527c704fe438b970d79df00e807e34
Thomas Müller <git@tom94.net>
2026-03-22 16:30:51 +0100

    feat(flake): add optional clang 22 direnv

--- flake.nix
+++ flake.nix
@@ -6,11 +6,35 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         package = pkgs.callPackage ./package.nix { };
+
+        ldLibraryPath = pkgs.lib.makeLibraryPath (
+          with pkgs;
+          [
+            wayland
+            libxkbcommon
+          ]
+        );
+
+        commonBuildInputs =
+          with pkgs;
+          if stdenv.isDarwin then
+            [ ]
+          else
+            [
+              binutils
+              mesa-demos # for glxinfo, eglinfo
+            ];
       in
       {
         packages = {
@@ -20,17 +44,32 @@
 
         devShells.default = pkgs.mkShell {
           inputsFrom = [ package ];
-          buildInputs = with pkgs; if stdenv.isDarwin then [ ] else [
-            gcc
-            gdb
-            binutils
-            mesa-demos # for glxinfo, eglinfo
-          ];
-          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (with pkgs; [
-            wayland
-            libxkbcommon
-          ]);
+          buildInputs =
+            commonBuildInputs
+            ++ (
+              with pkgs;
+              if stdenv.isDarwin then
+                [ ]
+              else
+                [
+                  gcc
+                  gdb
+                ]
+            );
+          LD_LIBRARY_PATH = ldLibraryPath;
         };
+
+        devShells.clang =
+          pkgs.mkShell.override
+            {
+              stdenv = pkgs.llvmPackages_22.libcxxStdenv;
+            }
+            {
+              inputsFrom = [ package ];
+              buildInputs =
+                commonBuildInputs ++ (with pkgs; if stdenv.isDarwin then [ ] else [ llvmPackages_22.lldb ]);
+              LD_LIBRARY_PATH = ldLibraryPath;
+            };
 
         apps.default = flake-utils.lib.mkApp {
           drv = package;
